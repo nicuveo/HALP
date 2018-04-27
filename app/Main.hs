86eef35c3ec08e1b73058b5cb4ff736c74df361f
Nicuvëo <crucuny@gmail.com>
2018-04-27 16:25:17 +0200

    Cleaning and renaming.

--- app/Main.hs
+++ app/Main.hs
@@ -42,13 +42,15 @@ tryAll :: [Parsec String () a] -> Parsec String () a
 tryAll parsers = foldl1 (<|>) $ map try parsers
 
 betweenSpaces :: Parser a -> Parser a
-betweenSpaces p = spaces *> p <* spaces
+betweenSpaces = between spaces spaces
 
 betweenParentheses :: Parser a -> Parser a
-betweenParentheses p = symbol "(" *> p <* symbol ")"
+betweenParentheses = between openParen closeParen
+  where openParen  = symbol "("
+        closeParen = symbol ")"
 
 symbol :: String -> Parsec String () String
-symbol s = betweenSpaces $ string s
+symbol = betweenSpaces . string
 
 
 
@@ -58,34 +60,34 @@ number :: Parser Int
 number = betweenSpaces $ read <$> many1 digit
 
 vector :: Parser Vector
-vector = betweenSpaces $ atom `sepBy1` spaces
+vector = betweenSpaces $ atom `sepEndBy1` spaces
 
 atom :: Parser Atom
 atom = tryAll [ N <$> number
               , E <$> betweenParentheses expr
               ]
 
-unOperator :: Parser UnaryOperator
-unOperator = tryAll [ symbol "p" >> return Rho
-                    ]
+unaryOperator :: Parser UnaryOperator
+unaryOperator = tryAll [ symbol "p" >> return Rho
+                       ]
 
-binOperator :: Parser BinaryOperator
-binOperator = tryAll [ symbol "+" >> return Plus
-                     , symbol "-" >> return Minus
-                     , symbol "x" >> return Product
-                     ]
+binaryOperator :: Parser BinaryOperator
+binaryOperator = tryAll [ symbol "+" >> return Plus
+                        , symbol "-" >> return Minus
+                        , symbol "x" >> return Product
+                        ]
 
 operation :: Parser Operation
 operation = tryAll [unOp, binOp]
-  where binOp = do
-          opLeft   <- vector
-          binaryOp <- binOperator
-          opRight  <- expr
-          return $ B binaryOp opLeft opRight
-        unOp = do
-          unaryOp  <- unOperator
+  where unOp = do
+          unaryOp  <- unaryOperator
           operand  <- expr
           return $ U unaryOp operand
+        binOp = do
+          opLeft   <- vector
+          binaryOp <- binaryOperator
+          opRight  <- expr
+          return $ B binaryOp opLeft opRight
 
 expr :: Parser Expression
 expr = tryAll [ fmap O operation
@@ -95,13 +97,13 @@ expr = tryAll [ fmap O operation
 
 -- compute
 
-unFunction :: UnaryOperator -> Value -> Value
-unFunction Rho = pure . length
+unaryFunction :: UnaryOperator -> Value -> Value
+unaryFunction Rho = pure . length
 
-binFunction :: BinaryOperator -> Value -> Value -> Value
-binFunction Plus    = zipWith (+)
-binFunction Minus   = zipWith (-)
-binFunction Product = zipWith (*)
+binaryFunction :: BinaryOperator -> Value -> Value -> Value
+binaryFunction Plus    = zipWith (+)
+binaryFunction Minus   = zipWith (-)
+binaryFunction Product = zipWith (*)
 
 computeAtom :: Atom -> Int
 computeAtom (N x) = x
@@ -109,8 +111,8 @@ computeAtom (E e) = head $ compute e
 
 compute :: Expression -> Value
 compute (V v)         = map computeAtom v
-compute (O (U o r))   = unFunction o $ compute r
-compute (O (B o l r)) = binFunction o (compute $ V l) $ compute r
+compute (O (U o r))   = unaryFunction o $ compute r
+compute (O (B o l r)) = binaryFunction o (compute $ V l) $ compute r
 
 eval :: String -> Value
 eval = compute . parseWith expr
@@ -127,8 +129,7 @@ repl = do
   putStr "λ "
   hFlush stdout
   line <- getLine
-  unless (null line || nub line == " ") $
-    print $ eval line
+  unless (null line || nub line == " ") $ print $ eval line
 
 main :: IO ()
 main = forever repl
